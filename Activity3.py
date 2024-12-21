7624deb39769052df06386f6f3ddab1f6100089b
Vivienne126 <vivienne.singh@cns.ac.in>
2024-12-21 11:44:21 +0530

    New2

---
+++ Activity3.py
@@ -0,0 +1,6 @@
+English=int(input("Enter your English marks"))
+Maths=int(input("Enter your maths marks"))
+Science=int(input("Enter your Science marks"))
+sum=English+Maths+Science
+Percentage=(sum/300)*100
+print("The percentage is", Percentage)
